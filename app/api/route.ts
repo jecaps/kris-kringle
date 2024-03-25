38d83577c7f48ca052e9ea09328362e603e5ba50
Jerika Kanz <jerikacapila@gmail.com>
2024-03-25 09:53:25 +0100

    Add api route for fetching all participants

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,19 @@
-import { assignSanta } from "@/lib/actions";
+import { assignSanta, getParticipants } from "@/lib/actions";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    const participants = await getParticipants();
+    return NextResponse.json(
+        {
+            result: participants,
+        },
+        {
+            status: 201,
+        }
+    );
+}
+
+export async function POST() {
     const msg = await assignSanta();
     return NextResponse.json(
         {
