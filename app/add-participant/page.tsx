8d6d13ff05c74d727c4fd669a71fa721fded0a80
Jerika Kanz <jerikacapila@gmail.com>
2024-03-28 14:12:02 +0100

    Update Participant Form

--- app/add-participant/page.tsx
+++ app/add-participant/page.tsx
@@ -1,11 +1,23 @@
-import ParticipantForm from "@/components/participant/partipant-form";
+import ParticipantForm from "@/components/participant/participant-form";
 
 export default function AddParticipantPage() {
     return (
-        <>
+        <div className="flex flex-column align-items-center justify-content-evenly mt-5 text-center">
             <h1>Join the exchanging of gifts!</h1>
-            <p>Enter your name and your wishlist</p>
+            <p className="text-gray-400">
+                Fill in the form below to participate in the gift exchange.
+                We&apos;re excited to have you join us!
+            </p>
             <ParticipantForm />
-        </>
+            <div className="mt-5 w-6">
+                <h2>What&apos;s next?</h2>
+                <p className="text-gray-300">
+                    After you&apos;ve added your name, you&apos;ll be included
+                    in the gift exchange. You&apos;ll receive an email with the
+                    name of the person you&apos;re buying a gift for, along with
+                    their wish list.
+                </p>
+            </div>
+        </div>
     );
 }
