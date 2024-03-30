e7dbaf0dc46e667016072dab0682eaa045db19d7
Jerika Kanz <jerikacapila@gmail.com>
2024-03-30 16:36:14 +0100

    Add more pages

---
+++ app/groups/[id]/join/page.tsx
@@ -0,0 +1,3 @@
+export default function JoinPage() {
+    return <div>JoinPage</div>;
+}
---
+++ app/groups/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function GroupPage({ params }: { params: { id: string } }) {
+    return <div>group {params.id}</div>;
+}
