948fe05640f37fe97c03e4d5c75117f00dcbeabe
Jerika Kanz <jerikacapila@gmail.com>
2024-05-02 15:37:25 +0200

    Change color theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-dark-blue/theme.css";
+import "primereact/resources/themes/lara-dark-teal/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
