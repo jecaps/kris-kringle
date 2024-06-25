d6c581993d822931eb961b913adc393f7369684c
Jerika Kanz <jerikanz@jkanz.de>
2024-06-25 16:21:37 +0200

    Update sendEmail

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,25 +39,13 @@ export async function sendEmail(groupId: string) {
         },
     });
 
-    // transporter.verify((error, success) => {
-    //     if (error) {
-    //         console.error("Connection error:", error);
-    //         return;
-    //     } else {
-    //         console.log("Server is ready to take our messages");
-    //     }
-    // });
-
-    await new Promise((resolve, reject) => {
-        transporter.verify(function (error, success) {
-            if (error) {
-                console.log(error);
-                reject(error);
-            } else {
-                console.log("Server is ready to take our messages");
-                resolve(success);
-            }
-        });
+    transporter.verify((error, success) => {
+        if (error) {
+            console.error("Connection error:", error);
+            return;
+        } else {
+            console.log("Server is ready to take our messages");
+        }
     });
 
     result.map(async (item) => {
@@ -109,6 +97,16 @@ export async function sendEmail(groupId: string) {
                 </div>
                 `,
         };
-        await transporter.sendMail(mailOptions);
+        // await transporter.sendMail(mailOptions);
+
+        return await new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(info);
+                }
+            });
+        });
     });
 }
