9ead0b7e5027b1fb3c09345424c4d76a5ff5f6fa
Jerika Kanz <jerikanz@jkanz.de>
2024-06-25 15:56:42 +0200

    Async transporter verification

--- lib/utils.ts
+++ lib/utils.ts
@@ -29,8 +29,6 @@ export async function sendEmail(groupId: string) {
     const user = process.env.EMAIL;
     const pass = process.env.PASSWORD;
 
-    console.log(user, pass);
-
     const transporter = nodemailer.createTransport({
         host: "smtp.strato.de",
         port: 465,
@@ -41,13 +39,25 @@ export async function sendEmail(groupId: string) {
         },
     });
 
-    transporter.verify((error, success) => {
-        if (error) {
-            console.error("Connection error:", error);
-            return;
-        } else {
-            console.log("Server is ready to take our messages");
-        }
+    // transporter.verify((error, success) => {
+    //     if (error) {
+    //         console.error("Connection error:", error);
+    //         return;
+    //     } else {
+    //         console.log("Server is ready to take our messages");
+    //     }
+    // });
+
+    await new Promise((resolve, reject) => {
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
     });
 
     result.map(async (item) => {
